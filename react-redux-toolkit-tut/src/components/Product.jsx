2e5e17e7818549243d21f00efcf75bcad0ae721c
Supreetha Kharvi <suprithakharvi99@gmail.com>
2026-05-29 12:47:27 +0530

    api showing

--- react-redux-toolkit-tut/src/components/Product.jsx
+++ react-redux-toolkit-tut/src/components/Product.jsx
@@ -1,23 +1,34 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addItem, removeItem } from "../store/slice"
-
+import { useEffect } from "react";
+import {fetchProduct} from "../store/productSlice"
 
 const Product = () => {
   const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchProduct())
+  },[])
+
+  const productSelector = useSelector((state)=>state.products.items);
+  console.log(productSelector);
+  
 
   return (
-    <div className='product-card'>
-        <div className="product-image">
-            <img src='https://m.media-amazon.com/images/I/61-g7m+90eL.jpg' alt='Product Image'/>
+    <div className='grid'>
+     {
+      productSelector.length && productSelector.map((item)=>(
+        <div className="card">
+          <img src={item.thumbnail}/>
+          <div className="content">
+            <div className="title">{item.title}</div>
+            <div className="brand">{item.brand}</div>
+            <div className="price">{item.price}</div>
+            <div className="rating">{item.rating}</div>
+            <button className="btn">Add To Cart</button>
+          </div>
         </div>
-        <div className="product-info">
-            <h1>Wireless Headphones</h1>
-            <p className='price'>$129.99</p>
-            <p className="description">Experience gigh-quality sound with these wireless headphones. Featuring noise cancellation, long-lasting battery life, and a sleek modern design for everyday use.
-            </p>
-            <button onClick={()=>dispatch(addItem(1))} className='btn'>Add to Cart</button>
-            <button onClick={()=>dispatch(removeItem(1))} className='btn remove-btn'>Remove From Cart</button>
-        </div>
+      ))
+     }
     </div>
   )
 }
--- react-redux-toolkit-tut/src/store/store.js
+++ react-redux-toolkit-tut/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slice'
+import productReducer from './productSlice'
 
 export const store = configureStore({
     reducer: {
-        cart:cartReducer
+        cart:cartReducer,
+        products:productReducer
     }
 })
